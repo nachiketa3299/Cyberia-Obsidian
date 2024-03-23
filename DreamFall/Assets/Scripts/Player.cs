d9f644f6802e3db9f4e1b6a1e4fd4a5d0ffff5fd
chunX2maru <netjin99@gmail.com>
2024-03-23 17:24:56 +0900

    Prototype 시작전

---
+++ DreamFall/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HellowWorld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
