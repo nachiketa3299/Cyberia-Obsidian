2bfe251d29c5ec3ef2d64c6a398a4b25937adecd
chunX2maru <netjin99@gmail.com>
2024-03-23 19:09:23 +0900

    기본 움직임

--- DreamFall/Assets/Scripts/Player.cs
+++ DreamFall/Assets/Scripts/Player.cs
@@ -1,17 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Animator playAnimator;
+    [SerializeField] private Transform playerTR;
+    private CharacterController controller;
+    private float speed = 5;
+    private float rotateSpeed = 10;
+
+    private Vector3 moveDir;
+    private bool isMove = false;
+
+    private void Awake()
+    {
+        playAnimator = GetComponentInChildren<Animator>();
+        playerTR = GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+    }
+
     void Start()
     {
         Debug.Log("HellowWorld");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        float horizon = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        moveDir = new Vector3(horizon, 0, Vertical);
+        if (moveDir == Vector3.zero)
+        {
+            isMove = false;
+            playAnimator.SetBool("isMove", isMove);
+            return;
+        }
+
+        isMove = true;
+        playAnimator.SetBool("isMove", isMove);
+        Debug.Log(moveDir);
+        moveDir = moveDir.normalized * speed * Time.deltaTime;
+        controller.Move(moveDir);
+        Quaternion rotation = Quaternion.LookRotation(moveDir);
+        playerTR.rotation = Quaternion.Slerp(playerTR.rotation, rotation, rotateSpeed * Time.deltaTime);
+
+        // if (Input.GetAxis("Horizontal") > 0f)
+        // { 
+        //     Debug.Log("R");
+        //     playerTR.position += new Vector3(1, 0, 0);
+        // }
+        // if (Input.GetAxis("Horizontal") < 0f)
+        // {
+        //     Debug.Log("L");
+        //     playerTR.position += new Vector3(-1, 0, 0);
+        // }
     }
 }
